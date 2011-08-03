9bee989238e6586c6d26c8ae4c83dd46114aacbc
Thomas Roehr <thomas.roehr@dfki.de>
2011-08-03 14:42:59 +0200

    Add the startup script for the message_producer_deployment

---
+++ scripts/start.rb
@@ -0,0 +1,22 @@
+require 'orocos'
+
+include Orocos
+Orocos.initialize
+
+Orocos.run 'message_producer_deployment' do 
+
+    message_producer = TaskContext.get 'message_producer'
+    
+    message_producer.start
+
+    reader = message_producer.messages.reader
+
+    while true
+        if msg = reader.read_new
+            puts "#{msg.time} #{msg.content}"
+        end 
+
+        sleep 0.5
+    end
+end
+
