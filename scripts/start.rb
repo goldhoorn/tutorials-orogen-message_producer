29cc04d25f2040052679fb204e4690066c851943
Thomas Roehr <thomas.roehr@dfki.de>
2011-08-04 10:52:33 +0200

    Enable nameservice for your component

--- scripts/start.rb
+++ scripts/start.rb
@@ -1,22 +1,22 @@
 require 'orocos'
+require 'readline'
 
 include Orocos
+Nameservice::enable(:AVAHI, { :searchdomains => [ '_robot._tcp'] })
 Orocos.initialize
 
-Orocos.run 'message_producer_deployment' do 
+Orocos.run 'message_producer_deployment' , 'cmdline_args' => { 'sd-domain' => '_robot._tcp'}, 'wait' => 3  do 
 
     message_producer = TaskContext.get 'message_producer'
-    
+
+    message_producer.config do |p|
+        p.uppercase = true
+    end
+
+    message_producer.configure    
     message_producer.start
 
-    reader = message_producer.messages.reader
-
-    while true
-        if msg = reader.read_new
-            puts "#{msg.time} #{msg.content}"
-        end 
-
-        sleep 0.5
+    Readline::readline("Press ENTER to exit\n") do
     end
 end
 
