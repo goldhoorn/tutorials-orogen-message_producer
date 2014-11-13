c6d93928852fc1853517d4667700d0a56c0e14a4
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-11-13 10:48:48 +0100

    Message producer example which includes dynamic-properties (for testing) and a task-scheduler. (made problems in the past, mentioned for debuging)

--- scripts/start.rb
+++ scripts/start.rb
@@ -3,19 +3,29 @@ require 'orocos'
 include Orocos
 Orocos.initialize
 
-Orocos.run 'message_producer::Task' => 'message_producer' do 
+Orocos.run 'sched' do 
 
-    message_producer = TaskContext.get 'message_producer'
-    
+    message_producer = TaskContext.get 'task1'
+    sched = TaskContext.get 'sched_scheduler'
+
+    message_producer.dynamic = 3.0
     message_producer.start
+    message_producer.dynamic = 1.0
+
+    sched.configure
+    message_producer.dynamic = 2.0
+    sched.execution_order = ['task1','task2']
+    sched.start
+    message_producer.dynamic = 6.0
 
     reader = message_producer.messages.reader
 
+    STDOUT.puts "Finished"
     while true
         if msg = reader.read_new
             puts "#{msg.time} #{msg.content}"
         end 
-
+        sched.trigger.write(1)
         sleep 0.5
     end
 end
